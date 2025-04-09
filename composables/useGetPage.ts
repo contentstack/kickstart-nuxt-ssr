a856fd3870b21abcc814adf0150bc374c9d3998d
Tim Benniks <tbenniks@gmail.com>
2025-04-09 12:47:29 +0200

    feat: initialize Nuxt SSR project with Contentstack integration
    - Add package.json for project configuration and dependencies
    - Create contentstack plugin for handling Contentstack SDK and live preview
    - Include favicon.ico and robots.txt in public directory
    - Set up TypeScript configuration files for server and project
    - Define types for PublishDetails, File, and Page interfaces

---
+++ composables/useGetPage.ts
@@ -0,0 +1,35 @@
+import contentstack, { QueryOperation, type LivePreviewQuery } from "@contentstack/delivery-sdk";
+import type { Page } from "~/types";
+
+export const useGetPage = async (url: string) => {
+  const { data, status } = await useAsyncData(`page-${url}`, async () => {
+    const { $preview, $stack } = useNuxtApp()
+    const route = useRoute()
+    const qs = toRaw(route.query)
+
+    if ($preview && qs?.live_preview) {
+      const route = useRoute()
+      const qs = toRaw(route.query)
+      $stack.livePreviewQuery(qs as unknown as LivePreviewQuery)
+    }
+
+    const result = await $stack
+      .contentType("page")
+      .entry()
+      .query()
+      .where("url", QueryOperation.EQUALS, url)
+      .find<Page>();
+
+    if (result.entries) {
+      const entry = result.entries[0]
+
+      if ($preview) {
+        contentstack.Utils.addEditableTags(entry as Page, 'page', true);
+      }
+
+      return entry;
+    }
+  });
+
+  return { data, status }
+}
---
+++ plugins/contentstack.ts
@@ -0,0 +1,57 @@
+import contentstack, { Region } from "@contentstack/delivery-sdk"
+import ContentstackLivePreview, { type IStackSdk } from "@contentstack/live-preview-utils";
+import { getContentstackEndpoints, getRegionForString } from "@timbenniks/contentstack-endpoints";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const {
+    apiKey,
+    deliveryToken,
+    previewToken,
+    region,
+    environment,
+    preview
+  } = nuxtApp.$config.public;
+
+  const regionEnum: Region = getRegionForString(region)
+  const endpoints = getContentstackEndpoints(regionEnum, true)
+
+  const stack = contentstack.stack({
+    apiKey,
+    deliveryToken,
+    environment,
+    region: regionEnum,
+    live_preview: {
+      enable: preview ? true : false,
+      preview_token: previewToken,
+      host: endpoints.preview
+    }
+  });
+
+  if (preview && import.meta.client) {
+    ContentstackLivePreview.init({
+      ssr: true,
+      mode: "builder",
+      enable: preview ? true : false,
+      stackSdk: stack.config as IStackSdk,
+      stackDetails: {
+        apiKey: apiKey,
+        environment: environment,
+      },
+      clientUrlParams: {
+        host: endpoints.application
+      },
+      editButton: {
+        enable: true,
+        exclude: ["outsideLivePreviewPortal"]
+      }
+    });
+  }
+
+  return {
+    provide: {
+      stack,
+      preview,
+      ContentstackLivePreview
+    },
+  };
+});
