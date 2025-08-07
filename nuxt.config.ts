517f9c192d862da9f20230026fbe4818efe203eb
Tim Benniks <tbenniks@gmail.com>
2025-08-07 14:09:26 +0200

    chore: works on non-prod env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { getContentstackEndpoints, getRegionForString } from "@timbenniks/contentstack-endpoints";
+const region = getRegionForString(process.env.NUXT_CONTENTSTACK_REGION as string)
+const endpoints = getContentstackEndpoints(region, true)
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
   modules: ['@nuxtjs/tailwindcss'],
-
-  build: {
-    transpile: ['@timbenniks/contentstack-endpoints']
-  },
-
   runtimeConfig: {
+    // Certain API endpoints can be set via environment variables for internal testing at Contentstack
+    // You can omit in your project. Use @timbenniks/contentstack-endpoints to get the right urls for your region.
     public: {
       apiKey: process.env.NUXT_CONTENTSTACK_API_KEY,
       deliveryToken: process.env.NUXT_CONTENTSTACK_DELIVERY_TOKEN,
       previewToken: process.env.NUXT_CONTENTSTACK_PREVIEW_TOKEN,
       environment: process.env.NUXT_CONTENTSTACK_ENVIRONMENT,
       preview: process.env.NUXT_CONTENTSTACK_PREVIEW === "true",
-      region: process.env.NUXT_CONTENTSTACK_REGION,
+      region: region ? region : process.env.NUXT_CONTENTSTACK_REGION as any,
+      contentDelivery: process.env.NUXT_CONTENTSTACK_CONTENT_DELIVERY || endpoints && endpoints.contentDelivery,
+      previewHost: process.env.NUXT_CONTENTSTACK_PREVIEW_HOST || endpoints && endpoints.preview,
+      applicationHost: process.env.NUXT_CONTENTSTACK_CONTENT_APPLICATION || endpoints && endpoints.application
     },
   },
 })
