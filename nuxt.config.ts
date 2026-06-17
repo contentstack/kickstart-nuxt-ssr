495dbe50f00363f2065775b802d2c2b3a0395f4d
raj pandey <raj.pandey@contentstack.com>
2026-06-17 14:21:39 +0530

    enhc: replace @timbenniks/contentstack-endpoints with @contentstack/utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { getContentstackEndpoints, getRegionForString } from "@timbenniks/contentstack-endpoints";
-const region = getRegionForString(process.env.NUXT_CONTENTSTACK_REGION as string)
-const endpoints = getContentstackEndpoints(region, true)
+import { getContentstackEndpoint, type ContentstackEndpoints } from "@contentstack/utils";
+const endpoints = getContentstackEndpoint(process.env.NUXT_CONTENTSTACK_REGION || 'us', '', true) as ContentstackEndpoints
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
-    // Certain API endpoints can be set via environment variables for internal testing at Contentstack
-    // You can omit in your project. Use @timbenniks/contentstack-endpoints to get the right urls for your region.
+    // Certain API endpoints can be set via environment variables for custom or dedicated Contentstack environments.
+    // You can omit these in your project. Use @contentstack/utils getContentstackEndpoint to get the right urls for your region.
     public: {
       apiKey: process.env.NUXT_CONTENTSTACK_API_KEY,
       deliveryToken: process.env.NUXT_CONTENTSTACK_DELIVERY_TOKEN,
       previewToken: process.env.NUXT_CONTENTSTACK_PREVIEW_TOKEN,
       environment: process.env.NUXT_CONTENTSTACK_ENVIRONMENT,
       preview: process.env.NUXT_CONTENTSTACK_PREVIEW === "true",
-      region: region ? region : process.env.NUXT_CONTENTSTACK_REGION as any,
-      contentDelivery: process.env.NUXT_CONTENTSTACK_CONTENT_DELIVERY || endpoints && endpoints.contentDelivery,
-      previewHost: process.env.NUXT_CONTENTSTACK_PREVIEW_HOST || endpoints && endpoints.preview,
-      applicationHost: process.env.NUXT_CONTENTSTACK_CONTENT_APPLICATION || endpoints && endpoints.application
+      region: process.env.NUXT_CONTENTSTACK_REGION as any,
+      contentDelivery: process.env.NUXT_CONTENTSTACK_CONTENT_DELIVERY || endpoints.contentDelivery as string,
+      previewHost: process.env.NUXT_CONTENTSTACK_PREVIEW_HOST || endpoints.preview as string,
+      applicationHost: process.env.NUXT_CONTENTSTACK_CONTENT_APPLICATION || endpoints.application as string
     },
   },
 })
